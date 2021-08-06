ebff603cc354a13d55b4db23f4cd99a3e458990f
Emily Xiong <xiongemi@gmail.com>
2021-08-05 23:50:52 -0400

    upgrade to webpack 5 and add a gatsby app

---
+++ apps/gatsby/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/emilyxiong/Code/tmp/nx-tap-error-recreate/apps/gatsby/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/emilyxiong/Code/tmp/nx-tap-error-recreate/apps/gatsby/src/pages/404.js")),
+  "component---src-pages-index-tsx": preferDefault(require("/Users/emilyxiong/Code/tmp/nx-tap-error-recreate/apps/gatsby/src/pages/index.tsx"))
+}
+
